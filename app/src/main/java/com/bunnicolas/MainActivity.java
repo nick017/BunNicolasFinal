9d6ae7db4b5ece95f030b2f37e70180cde651db1
Nicolas Stanely <t.simon080@gmail.com>
2020-07-22 16:24:49 +0700

    Second Progress Commit

--- app/src/main/java/com/bunnicolas/MainActivity.java
+++ app/src/main/java/com/bunnicolas/MainActivity.java
@@ -1,14 +1,52 @@
 package com.bunnicolas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        showFragement(new FragmentHome());
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.tab_home:
+                        showFragement(new FragmentHome());
+                        break;
+                    case R.id.tab_categories:
+                        showFragement(new FragmentCategories());
+                        break;
+                    case R.id.tab_search:
+                        showFragement(new FragmentSearch());
+                        break;
+                    case R.id.tab_account:
+                        showFragement(new FragmentAccount());
+                        break;
+                }
+                return false;
+            }
+
+        });
+    };
+
+    public void showFragement(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.layout_fragment, fragment);
+        transaction.commit();
     }
 }
